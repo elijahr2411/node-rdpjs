f89c62e84d4cd0e91a3df93f73ca03076727f27f
Elijah R <elijah@computernewb.com>
2023-12-04 19:28:41 -0500

    update typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -9,6 +9,10 @@ declare class RdpClient extends EventEmitter {
     sendKeyEventScancode(code: number, isPressed: boolean, extended?: boolean): void;
     sendKeyEventUnicode(code: number, isPressed: boolean): void;
     sendWheelEvent(x: number, y: number, step: number, isNegative: boolean, isHorizontal: boolean): void;
+    on(event: 'connect', listener: () => void): this;
+    on(event: 'close', listener: () => void): this;
+    on(event: 'error', listener: (err: Error) => void): this;
+    on(event: 'bitmap', listener: (bitmap: RDPBitmap) => void): this;
 }
 
 declare function createClient(config: RDPClientConfig): RdpClient;
@@ -34,6 +38,18 @@ declare interface RDPClientConfig {
     screen ?: { width: number, height: number };
     locale ?: string;
     logLevel ?: string;
+}
+
+declare interface RDPBitmap {
+    destTop : number;
+    destLeft : number;
+    destBottom : number;
+    destRight : number;
+    width : number;
+    height : number;
+    bitsPerPixel : number;
+    isCompressed : boolean;
+    data : Uint8ClampedArray;
 }
 
 export {
